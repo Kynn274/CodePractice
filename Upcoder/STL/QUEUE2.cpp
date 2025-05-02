01c508d6acb1039e548f1070486f37cb8ba30ff3
Kynn274 <117021914+Kynn274@users.noreply.github.com>
2025-05-02 16:15:57 +0700

    Update and rename QUEUE2_uncheck.cpp to QUEUE2.cpp

--- Upcoder/STL/QUEUE2.cpp
+++ Upcoder/STL/QUEUE2.cpp
@@ -35,3 +35,19 @@ int main(){
     
     return 0;
 }
+
+/*
+Đúng 10/10
+#######################
+Thông tin chi tiết
+test 1 :	0.007370s 			0 MB		 [đúng]
+test 2 :	0.006210s 			0 MB		 [đúng]
+test 3 :	0.006673s 			0 MB		 [đúng]
+test 4 :	0.009077s 			0 MB		 [đúng]
+test 5 :	0.009072s 			0 MB		 [đúng]
+test 6 :	0.009290s 			0 MB		 [đúng]
+test 7 :	0.010386s 			0 MB		 [đúng]
+test 8 :	0.011162s 			0 MB		 [đúng]
+test 9 :	0.012710s 			0 MB		 [đúng]
+test 10 :	0.013299s 			0 MB		 [đúng]
+*/
